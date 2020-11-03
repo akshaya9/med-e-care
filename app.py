19d7c7bc2a66887c783407b7fcad8eae4915d663
akshaya9 <sunkariakshaya3890@gmail.com>
2020-11-04 01:22:53 +0530

    3

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 
+from wtform_fields import *
+
 app  = Flask(__name__)
 app.secret_key = 'should be changed'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    return render_template("index.html")
+
+    reg_form = RegistrationForm()
+    if reg_form.validate_on_submit():
+        return "Great success!"
+
+
+    return render_template("index.html", form=reg_form)
     
 #always true
 if __name__ == "__main__":
