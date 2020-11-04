395d36e9d32735392684069ba241c8168fbc98f7
akshaya9 <sunkariakshaya3890@gmail.com>
2020-11-04 08:38:45 +0530

    dashboard error

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from passlib.hash import pbkdf2_sha256 #password based key dervation func version2 for hashing
+from flask_login import LoginManager,login_user, current_user, login_required, logout_user
 from wtform_fields import *
 from models import *
 
@@ -8,6 +9,14 @@ app.secret_key = 'should be changed'
 
 app.config['SQLALCHEMY_DATABASE_URI']='postgres://gkuebhlmxhtjau:5e496cb9299ee89363d408be21367dab4dd1745fb45bed374f8a787bad39fd7c@ec2-52-2-82-109.compute-1.amazonaws.com:5432/d349hchhak43qj'
 db = SQLAlchemy(app)
+
+#Configure flask login
+login = LoginManager(app)
+login.init_app(app)
+
+@login.user_loader(id)
+def load_user(id):
+    return User.query.get(int(id))
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -33,10 +42,27 @@ def login():
     login_form = LoginForm()
     #allows login if validation is sucessfull
     if login_form.validate_on_submit():
-        return "Logged in successfully!!"
+        user_object = User.query.filter_by(username=login_form.username.data).first()
+        login_user(user_object)
+        return redirect(url_for('dashboard'))
+
     return render_template("login.html", form=login_form)
 
-    
+@app.route("/dashboard", methods=['GET', 'POST'])
+# @login_required
+def dashboard():
+    # if not current_user.is_authenticated:
+    #     return current_app.login_manager.unauthorized()
+    return "Welcome to dashboard!"
+
+
+@app.route("/logout", methods=['GET'])
+def logout():
+    logout_user()
+    return "You've logged out successfully!"
+
+
+
 #always true
 if __name__ == "__main__":
     app.run(debug=True)
